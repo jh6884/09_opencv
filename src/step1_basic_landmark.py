c1efed68f079f11d3f81d14d4e671d4693970ed3
shinjaehyeon <pumpkinalpha996@gmail.com>
2025-08-07 16:57:10 +0900

    파일명 변경, fps를 측정하기 위한 코드 삽입

--- src/step1_basic_landmark.py
+++ src/step1_basic_landmark.py
@@ -1,9 +1,11 @@
-import cv2
+import cv2, time
 import dlib
 
 # 얼굴 검출기와 랜드마크 검출기 생성 --- ①
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('./shape_predictor_68_face_landmarks.dat')
+
+prev_time = 0
 
 cap = cv2.VideoCapture(0)
 
@@ -11,6 +13,13 @@ while cap.isOpened():
     ret, img = cap.read()
     if not ret:
         print('no frame.');break
+    
+    curr_time = time.time()
+    sec = curr_time - prev_time
+    fps = 1 / sec
+    str_fps = "FPS : %0.1f" % fps
+    cv2.putText(img, str_fps, (0, 30), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0))
+    
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # 얼굴 영역 검출 --- ②
     faces = detector(gray)
@@ -25,8 +34,8 @@ while cap.isOpened():
         for i in range(68):
             # 부위별 좌표 추출 및 표시 --- ⑤
             part = shape.part(i)
-            cv2.circle(img, (part.x, part.y), 2, (0, 0, 255), -1)
-#            cv2.putText(img, str(i), (part.x, part.y), cv2.FONT_HERSHEY_PLAIN, 0.5,(255,255,255), 1, cv2.LINE_AA)
+#            cv2.circle(img, (part.x, part.y), 2, (0, 0, 255), -1)
+            cv2.putText(img, str(i), (part.x, part.y), cv2.FONT_HERSHEY_PLAIN, 0.5,(255,255,255), 1, cv2.LINE_AA)
     
     cv2.imshow("face landmark", img)
     if cv2.waitKey(1)== 27:
